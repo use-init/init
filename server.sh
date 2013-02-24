88e58abc0e68140dd740f99054eac31b1b826d6b
Kahlil Lechelt <kahlil@kahlil.co>
2013-02-24 17:03:33 +0100

    Delete WEBrick server

--- server.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/sh
-port="${3000:-8000}"
-open -g "http://localhost:$port"
-ruby -r webrick -e "s = WEBrick::HTTPServer.new(:Port => $port, :DocumentRoot => Dir.pwd); trap('INT') { s.shutdown }; s.start"
