f51b133b4c5243a7c91e648bae06ac353fe96b65
Hans Christian Reinl <info@drublic.de>
2013-08-18 17:47:15 +0200

    Add example test specification

---
+++ tests/spec.js
@@ -0,0 +1,21 @@
+require(['modules/module'], function (module) {
+	'use strict';
+
+	describe('init', function () {
+		it('is available', function () {
+			expect(module).not.toBe(null);
+		});
+
+		it('fires event on init', function () {
+			var eventCalled;
+
+			$(document).on(module.getEventName(), function () {
+				eventCalled = true;
+			});
+
+			module.init();
+
+			expect(eventCalled).toBeTruthy();
+		});
+	});
+});
