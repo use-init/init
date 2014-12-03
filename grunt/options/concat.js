bec858887a18568bc6bfc09c8ca719e9c889afb3
Anselm Hannemann <info@anselm-hannemann.com>
2014-12-04 00:24:22 +0100

    Automatically name page files by object key.
    This removes the `dest` key in the pages.json
    and retrieves the filename automatically.
    
    Unfortunately this adds another dependency as
    we need to deep-extend the object but I think
    this is worth it.
    
    This is part of #131 and targets #123.

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -4,7 +4,17 @@
 'use strict';
 
 var xtend = require('xtend');
+var deepExtend = require('deep-extend');
 var pages = require('../../pages');
 var config = require('../config');
+var pageName;
+var pagePath;
+
+// Extend the pages object with the destination path of the page
+for (pageName in pages) {
+  pagePath = 'temp/' + pageName + '.html';
+
+  deepExtend(pages[pageName], { 'dest': pagePath });
+}
 
 module.exports = xtend(pages, config.concat || {});
